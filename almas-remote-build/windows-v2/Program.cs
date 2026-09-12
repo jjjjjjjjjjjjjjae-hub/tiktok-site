4e2d4a614636fc318d7a5a43fb3e2d88cb27b72d
jjjjjjjjjjjjjjae-hub <jjjjjjjjjjjjjjae@gmail.com>
2026-09-12 13:30:41 +0500

    Add V2 Windows entry point

---
+++ almas-remote-build/windows-v2/Program.cs
@@ -0,0 +1,11 @@
+namespace AlmasRemoteV2;
+
+internal static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
